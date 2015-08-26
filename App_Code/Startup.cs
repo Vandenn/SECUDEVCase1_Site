c083046ddd59f2cc9ce3bce52380f39fe300650b
Vandenn <evan.livelo@gmail.com>
2015-08-26 14:59:48 +0800

    Initial Push

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SECUDEVCase1_Site.Startup))]
+namespace SECUDEVCase1_Site
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
